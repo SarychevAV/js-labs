46618aff768af9b0be12540c557127ea2c03db80
PiFFFko <piwen.roman@yandex.ru>
2020-03-09 15:31:11 +0300

    Сделал второе задание

---
+++ laba 4/4.2/script.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+var canvas = document.getElementById("canv")
+var ctx = canvas.getContext('2d')
+canvas.height = 300
+canvas.width = 200
+var centerX = canvas.width / 2
+var centerY = canvas.height / 2
+var a = 40
+var b = 60
+
+//тут должны отрисовываться полуоси
+
+
+function plot() {
+ctx.clearRect(0,0,canvas.width,canvas.height);
+a = document.getElementById("a").value;
+b = document.getElementById("b").value;	
+ctx.save();
+ctx.beginPath();
+ctx.translate(centerX, centerY);
+ctx.moveTo(2*a,0);
+ctx.lineTo(-2*a,0);
+ctx.moveTo(0,2*b);
+ctx.lineTo(0,-b*2);
+ctx.stroke();
+ctx.restore();
+ctx.closePath();
+ctx.stroke();
+	
+ctx.save();
+ctx.beginPath();
+ctx.translate(centerX, centerY);
+ctx.scale(a / b, 1);
+ctx.arc(0, 0, b, 0, Math.PI * 2, true);
+ctx.restore();
+ctx.closePath();
+ctx.stroke();	
+}
